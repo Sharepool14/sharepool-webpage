4847ae1cb86055628b3f77d869002b718ec4872a
Theo Johansson <32549413+TheoJ2001@users.noreply.github.com>
2022-03-28 14:44:27 +0200

    removed svelte boilerplate, started work on navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+	import '../app.scss';
 </script>
 
-<Header />
 
 <main>
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
 
-<style>
+<style lang="scss">
 	main {
 		flex: 1;
 		display: flex;
@@ -25,21 +20,7 @@
 		box-sizing: border-box;
 	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
 	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
+	
 	}
 </style>
